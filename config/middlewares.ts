da8165edeef4676db1ec2e02adc3a2efa1231700
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-12-11 15:34:58 +0530

    first commit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,24 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", env("SUPABASE_URL")],
+          "media-src": ["'self'", "data:", "blob:", env("SUPABASE_URL")],
+        },
+      },
+    },
+  },
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
 export default () => ({});
+
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "supabase",
+//       providerOptions: {
+//         supabaseUrl: env("SUPABASE_URL"),
+//         supabaseKey: env("SUPABASE_KEY"),
+//         bucketName: env("SUPABASE_BUCKET"),
+//       },
+//     },
+//   },
+// });
